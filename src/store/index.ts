8d8775446aec053fc25966fc3c296645904011f1
JosianeReis <josiane-reis@outlook.com>
2023-09-03 13:07:31 +0100

    delete: removes todo feture added for test redux initial config

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from '../features/auth/auth';
 import charactersSlice from '../features/characters';
-import todoSlice from '../features/todo/todoSlice';
 
 export const store = configureStore({
   reducer: {
-    todo: todoSlice,
     auth: authSlice,
     characters: charactersSlice
   }
